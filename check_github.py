f4acc7372a67be2f61630492227f554e810159a1
Aran Khanna <arankhanna@college.harvard.edu>
2016-01-25 22:20:27 -0500

    Update check_github.py

--- check_github.py
+++ check_github.py
@@ -1,6 +1,19 @@
 import os
 import json
 import github
+import pymongo
+import urllib
+
+BASE_URL = 'https://github.com'
+
+# Pull all languages we are searching on into an array from the language file
+def get_file(l_file):
+	with open(l_file, "r") as p_file:
+	    array = []
+	    for cur_line in p_file:
+	        array.append(urllib.quote_plus(cur_line.rstrip('\n')))
+	return array
+
 
 #formats the secret line which we've read from the repo by removing leading + and -, tabs, and newlines
 def clean_secret_line(line):
@@ -11,38 +24,20 @@ def clean_secret_line(line):
 
 	#removes leading tabs
 	line = line.lstrip()
+
 	line = line.replace("\n", "")
 
 	return line
-
-def secrets_in_line(line, secrets, stopper_words=[]):
-    """
-    line is the input line
-    secrets is a list of search terms
-    stopper_words is a list of common placeholders
-    returns True if an = sign exists (assignment) 
-    OR any of the secrets are in the line
-    AND none of the stopper words are in the line 
-    """
-    # check if equal signs is 
-    flag = '=' in line
-    for secret in secrets:
-        flag = flag or (secret in line)  
-    # check for stopper words
-    for stopper_word in stopper_words:
-        if stopper_word in line:
-            return False
-    return flag
-
-# exclude things that look like environment variables 
-def is_environment_var(line):
-    """
-    True if looks like setting to environment variable
-    """
-    return  ("env" in line) or ("ENV" in line)
+	
+def is_in_db(query, language):
+	client = pymongo.MongoClient("mongodb://aran:aran1025@ds047020.mongolab.com:47020/personal-analytics")
+	db = client.get_default_database()
+	repos = db['repo_stats'].find_one({'search':query, 'language':language})
+	return repos
 
 #checks the given repo URL for values of the given secret variable
-def check_repo(url, secrets):
+def check_repo(url, secret):
+	print "checking "+url+" with secret "+secret
 	try:
 		#clone the repo			
 		os.system("git clone " + url)
@@ -59,10 +54,8 @@ def check_repo(url, secrets):
 		#find the lines containing secret
 		with open("secret.txt") as f:
 			lines = f.readlines()		
-        
-		secret_lines = [line for line in lines if secrets_in_line(line, secrets)]
-        # filter lines that might be environment variables
-        secret_lines = [line for line in secret_lines if not is_environment_var(line)]
+
+		secret_lines = [line for line in lines if secret in line]
 
 		os.chdir("../")
 		os.system("rm -rf " + name)
@@ -86,7 +79,9 @@ def check_urls(urls):
 			#	query
 			#	data
 			#		list of passwords
+
 			url, query, metadata = url_object
+
 			print "Checking ", url, query
 
 			outputfile.write(url + "\n")
@@ -99,53 +94,20 @@ def check_urls(urls):
 
 			outputfile.write("\n")
 
-'''
-urls = [
-	["https://github.com/ooola/ruggerfest", "SECRET_KEY", {"data": "test"}],
-	["https://github.com/sqs/openelections", "WEBAUTH_SECRET", {"data": "test"}],
-	["https://github.com/priestc/flightloggin2", "AWS_SECRET_KEY", {"data": "test"}],
-	["https://github.com/hilliard/basic-auth", "GMAIL_PASSWORD", {"data": "test"}],
-	["https://github.com/hecontreraso/crackplan", "GMAIL_PASSWORD", {"data": "test"}],
-	["https://github.com/gdb/domaincli", "stripe_api_key", {"data": "test"}],
-	["https://github.com/frank2workd/stripebank", "Stripe.api_key", {"data": "test"}],
-	["https://github.com/bcackerman/stripeanalytics", "Stripe.api_key", {"data": "test"}],	
-]
-'''
+languages = get_file("all_languages.txt")
+searches = get_file("searches.txt")
+print len(languages)
+print len(searches)
+
+for query in searches:
+	for language in languages:
+		repos = is_in_db(query, "PHP")
+		for link in repos['links']:
+			print check_repo(BASE_URL+link[0]+'.git', query)
+		break
+	break
 
 
-#urls = [['https://github.com/zdot/django-boilerplate.git', 'SECRET_KEY', {'commits': 6, 'branches': 1}], ['https://github.com/imenetoumi/www_auf_org.git', 'SECRET_KEY', {'commits': 22, 'branches': 1}], ['https://github.com/robrechtdr/djangobox.git', 'SECRET_KEY', {'commits': 42, 'branches': 1}], ['https://github.com/robrechtdr/djangobox.git', 'SECRET_KEY', {'commits': 42, 'branches': 1}], ['https://github.com/ernestjumbe/django-bootstrap-template.git', 'SECRET_KEY', {'commits': 55, 'branches': 1}], ['https://github.com/yaph/flask-init.git', 'SECRET_KEY', {'commits': 22, 'branches': 1}], ['https://github.com/meowfreeze/django_startproject.git', 'SECRET_KEY', {'commits': 57, 'branches': 3}], ['https://github.com/ernestjumbe/django-scaffold.git', 'SECRET_KEY', {'commits': 49, 'branches': 1}], ['https://github.com/cottonwoodcoding/charles_ellsworth.git', 'SECRET_KEY', {'commits': 91, 'branches': 2}], ['https://github.com/ulope/django-template.git', 'SECRET_KEY', {'commits': 8, 'branches': 1}], ['https://github.com/justuswilhelm/senior-residence.git', 'SECRET_KEY', {'commits': 182, 'branches': 1}], ['https://github.com/bkuri/scatpod.git', 'SECRET_KEY', {'commits': 33, 'branches': 2}], ['https://github.com/polakj/Portfolio.git', 'SECRET_KEY', {'commits': 17, 'branches': 1}], ['https://github.com/marazmiki/django-project-template.git', 'SECRET_KEY', {'commits': 31, 'branches': 1}], ['https://github.com/PostTenebrasLab/PTL-Status-API.git', 'SECRET_KEY', {'commits': 70, 'branches': 1}], ['https://github.com/McPants/portfolio.git', 'SECRET_KEY', {'commits': 14, 'branches': 1}]]
-
-urls = github.pull_results("SECRET_KEY")
-print urls
-
-#urls = [['https://github.com/zdot/django-boilerplate.git', 'SECRET_KEY', {'commits': 6, 'branches': 1}], ['https://github.com/imenetoumi/www_auf_org.git', 'SECRET_KEY', {'commits': 22, 'branches': 1}], ['https://github.com/robrechtdr/djangobox.git', 'SECRET_KEY', {'commits': 42, 'branches': 1}], ['https://github.com/robrechtdr/djangobox.git', 'SECRET_KEY', {'commits': 42, 'branches': 1}], ['https://github.com/ernestjumbe/django-bootstrap-template.git', 'SECRET_KEY', {'commits': 55, 'branches': 1}], ['https://github.com/yaph/flask-init.git', 'SECRET_KEY', {'commits': 22, 'branches': 1}], ['https://github.com/meowfreeze/django_startproject.git', 'SECRET_KEY', {'commits': 57, 'branches': 3}], ['https://github.com/ernestjumbe/django-scaffold.git', 'SECRET_KEY', {'commits': 49, 'branches': 1}], ['https://github.com/cottonwoodcoding/charles_ellsworth.git', 'SECRET_KEY', {'commits': 91, 'branches': 2}], ['https://github.com/ulope/django-template.git', 'SECRET_KEY', {'commits': 8, 'branches': 1}], ['https://github.com/justuswilhelm/senior-residence.git', 'SECRET_KEY', {'commits': 182, 'branches': 1}], ['https://github.com/bkuri/scatpod.git', 'SECRET_KEY', {'commits': 33, 'branches': 2}], ['https://github.com/marazmiki/django-project-template.git', 'SECRET_KEY', {'commits': 31, 'branches': 1}], ['https://github.com/polakj/Portfolio.git', 'SECRET_KEY', {'commits': 17, 'branches': 1}], ['https://github.com/PostTenebrasLab/PTL-Status-API.git', 'SECRET_KEY', {'commits': 70, 'branches': 1}], ['https://github.com/McPants/portfolio.git', 'SECRET_KEY', {'commits': 14, 'branches': 1}], ['https://github.com/kazu634/chef.git', 'SECRET_KEY', {'commits': 738, 'branches': 9}], ['https://github.com/justuswilhelm/senior-residence.git', 'SECRET_KEY', {'commits': 182, 'branches': 1}], ['https://github.com/bkuri/scatpod.git', 'SECRET_KEY', {'commits': 33, 'branches': 2}], ['https://github.com/marazmiki/django-project-template.git', 'SECRET_KEY', {'commits': 31, 'branches': 1}], ['https://github.com/polakj/Portfolio.git', 'SECRET_KEY', {'commits': 17, 'branches': 1}], ['https://github.com/PostTenebrasLab/PTL-Status-API.git', 'SECRET_KEY', {'commits': 70, 'branches': 1}], ['https://github.com/McPants/portfolio.git', 'SECRET_KEY', {'commits': 14, 'branches': 1}], ['https://github.com/kazu634/chef.git', 'SECRET_KEY', {'commits': 738, 'branches': 9}], ['https://github.com/pyprism/Hiren-Books.git', 'SECRET_KEY', {'commits': 104, 'branches': 1}], ['https://github.com/minimill/cerberus.git', 'SECRET_KEY', {'commits': 28, 'branches': 1}], ['https://github.com/LaMustax/skilleton.git', 'SECRET_KEY', {'commits': 11, 'branches': 1}], ['https://github.com/danrschlosser/isaaclevien.com.git', 'SECRET_KEY', {'commits': 8, 'branches': 1}], ['https://github.com/y2bishop2y/vagrant.flask.git', 'SECRET_KEY', {'commits': 5, 'branches': 1}], ['https://github.com/A-Estudiar/aestudiar_backend.git', 'SECRET_KEY', {'commits': 11, 'branches': 1}], ['https://github.com/tobbez/lys-reader.git', 'SECRET_KEY', {'commits': 118, 'branches': 1}], ['https://github.com/LukeHoersten/snaplet-postmark.git', 'SECRET_KEY', {'commits': 7, 'branches': 1}], ['https://github.com/greenmoon55/cntrains.git', 'SECRET_KEY', {'commits': 79, 'branches': 1}], ['https://github.com/MapStory/mapstory-geonode.git', 'SECRET_KEY', {'commits': 556, 'branches': 9}], ['https://github.com/nickray22/lafitte_mop.git', 'SECRET_KEY', {'commits': 5, 'branches': 1}], ['https://github.com/logandhead/flask-vagrant-puppet-boilerpate.git', 'SECRET_KEY', {'commits': 4, 'branches': 1}], ['https://github.com/theowni/encryptedSession-PHP.git', 'SECRET_KEY', {'commits': 3, 'branches': 1}], ['https://github.com/shea256/flask-app-generator.git', 'SECRET_KEY', {'commits': 40, 'branches': 1}], ['https://github.com/henriquebastos/dj-kickstart.git', 'SECRET_KEY', {'commits': 20, 'branches': 1}], ['https://github.com/catalyst/basil.git', 'SECRET_KEY', {'commits': 103, 'branches': 2}], ['https://github.com/clayman74/essential.git', 'SECRET_KEY', {'commits': 29, 'branches': 2}], ['https://github.com/matheusho/eventex.git', 'SECRET_KEY', {'commits': 83, 'branches': 1}], ['https://github.com/deis/example-spree.git', 'SECRET_KEY', {'commits': 1, 'branches': 1}], ['https://github.com/voltnor/pyChaos.git', 'SECRET_KEY', {'commits': 8, 'branches': 1}], ['https://github.com/develersrl/shorturl.git', 'SECRET_KEY', {'commits': 43, 'branches': 1}], ['https://github.com/jetsgit/fedtax.git', 'SECRET_KEY', {'commits': 3, 'branches': 1}], ['https://github.com/anaerobeth/dev-group-project.git', 'SECRET_KEY', {'commits': 16, 'branches': 1}]]
-
-check_urls(urls)
-
-#print check_repo()
-#print check_repo("https://github.com/ooola/ruggerfest", "SECRET_KEY")
-#print check_repo("https://github.com/sqs/openelections", "WEBAUTH_SECRET")
-#print check_repo("https://github.com/elections/openelections", "WEBAUTH_SECRET")
-
-#this repo has thousands of flight details, and contains a secret key
-#print check_repo("https://github.com/priestc/flightloggin2", "AWS_SECRET_KEY")
-
-#these repos have a gmail email and password
-#print check_repo("https://github.com/hilliard/basic-auth", "GMAIL_PASSWORD")
-#print check_repo("https://github.com/hecontreraso/crackplan", "GMAIL_PASSWORD")
-
-#this has a Stripe API key, but may be garbage value
-#print check_repo("https://github.com/gdb/domaincli", "stripe_api_key")
-
-#this has a Stripe API key, but are sk_test_XXXXXX values
-#print check_repo("https://github.com/frank2workd/stripebank", "Stripe.api_key")
-#print check_repo("https://github.com/bcackerman/stripeanalytics", "Stripe.api_key")
-
-
-
-
-
-
-
+# check_urls(urls)
 
 
